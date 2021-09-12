d14676640f3f044db766ada19b845c9b8a80d766
AEUHHH <dr.codycash@gmail.com>
2021-09-12 01:11:30 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Cody Kalczuk
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter a noun:");
+        String noun = input.nextLine();
+
+        System.out.println("Enter a verb:");
+        String verb = input.nextLine();
+
+        System.out.println("Enter an adjective:");
+        String adjective = input.nextLine();
+
+        System.out.println("Enter an adverb:");
+        String adverb = input.nextLine();
+
+        System.out.println("Do you ever " + verb + " outside with a " + adjective + " " + noun + " " + adverb + "? I know I do!");
+    }
+}
